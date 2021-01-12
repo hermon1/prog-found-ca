80bafe8f80204aaad95b0330fe42aa22e6d521dc
hermon1 <34695700+hermon1@users.noreply.github.com>
2021-01-12 17:50:58 +0100

    Add files via upload

---
+++ level-1.js
@@ -0,0 +1,102 @@
+// question 1
+var pet;
+
+// question 2
+var person= { 
+    name: "Olu", 
+    age: "20", 
+    }; 
+
+// question 3
+
+var outOfStock = true; 
+ 
+if(outOfStock === true) { 
+console.log("out of stock"); 
+} 
+else { 
+console.log("in stock"); 
+} 
+
+// question 4
+var digits= [1, 2, 2, 4, 5]; 
+
+console.log(digits[0]); 
+console.log(digits[1]); 
+console.log(digits[2]); 
+console.log(digits[3]); 
+
+console.log(digits[4]); 
+
+var digits = digits.length; 
+console.log(numberOfdigits) 
+
+for(var i = 0; i < numberOfdigits; i++) { 
+console.log(digits[i]); 
+} 
+
+// question 5
+
+for(var i = 15; i <= 25; i++) { 
+    console.log(i); 
+    } 
+
+
+// question 6
+
+for(var i = 15; i === 20; i++) { 
+    console.log(i); 
+    } 
+
+// question 7
+var person= [{ 
+    name: "Mathias", 
+    age: 20, 
+    student: true 
+    }, 
+     
+    { 
+    name: "Olu", 
+    age: 22, 
+    student: true 
+    } 
+    ]; 
+    
+    for(var i = 0; i < posts.length; i++) { 
+    
+     
+    console.log(posts[i].age); 
+     
+    console.log(posts[i].student); 
+     
+    
+     
+
+// question 8
+
+function whatIDontLike (dislike) { 
+    console.log("i dont like " + dislike); 
+    } 
+    whatIDontLike("giraffes"); 
+
+// question 9 
+
+function AddTwoTitles(title1, title2) { 
+    var sum = title1 - title2; 
+    console.log(sum); 
+    } 
+     
+
+// question 10
+
+var emptyArray = []; 
+
+function logWord(theWord);}
+
+emptyArray.push(theWord) 
+
+{ console.log(""); } 
+
+logWord(theWord);
+// call the function logWord(theWord); 
+ 
